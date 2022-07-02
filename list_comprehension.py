bbb154d8df97ec1e31875edf00af13e06deeee3b
Krang <108150590+Krang123@users.noreply.github.com>
2022-07-02 20:11:13 +0200

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,2 +1,12 @@
 string ="hello world"
 print([i for i in string])
+
+
+symbols ='$^*ù!'
+codes =[]
+for symbol in symbols:
+    codes.append(ord(symbol))
+print(codes)
+
+[ord(i) for i in symbols]
+
